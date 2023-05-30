4c5a39fac8b61d8e62608bfb2e2b835d6dc7b752
Mireia Olivé <mirisfu@gmail.com>
2023-05-30 21:27:33 +0200

    exercici-9 rutas homeCo

--- src/app/home/componentes/panell/panell.component.ts
+++ src/app/home/componentes/panell/panell.component.ts
@@ -1,49 +1,49 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { ResultService } from '../../services/result.service';
+import { Component, EventEmitter, Output } from "@angular/core";
+import { ResultService } from "../../services/result.service";
 
 @Component({
-  selector: 'app-home-panell',
-  templateUrl: './panell.component.html',
-  styleUrls: ['./panell.component.css'],
+    selector: "app-home-panell",
+    templateUrl: "./panell.component.html",
+    styleUrls: ["./panell.component.css"],
 })
 export class PanellComponent {
-  numeroPaginas: number = 0;
-  numeroIdiomas: number = 0;
+    numeroPaginas: number = 0;
+    numeroIdiomas: number = 0;
 
-  @Output() precioActualizado: EventEmitter<number> =
-    new EventEmitter<number>();
+    @Output() precioActualizado: EventEmitter<number> =
+        new EventEmitter<number>();
 
-  constructor(private resultService: ResultService) {}
+    constructor(private resultService: ResultService) {}
 
-  incrementarPaginas() {
-    this.numeroPaginas++;
-    this.actualizarPrecio();
-  }
-
-  decrementarPaginas() {
-    if (this.numeroPaginas > 0) {
-      this.numeroPaginas--;
-      this.actualizarPrecio();
+    incrementarPaginas() {
+        this.numeroPaginas++;
+        this.actualizarPrecio();
     }
-  }
 
-  incrementarIdiomas() {
-    this.numeroIdiomas++;
-    this.actualizarPrecio();
-  }
-
-  decrementarIdiomas() {
-    if (this.numeroIdiomas > 0) {
-      this.numeroIdiomas--;
-      this.actualizarPrecio();
+    decrementarPaginas() {
+        if (this.numeroPaginas > 0) {
+            this.numeroPaginas--;
+            this.actualizarPrecio();
+        }
     }
-  }
 
-  private actualizarPrecio() {
-    const precioTotal = this.resultService.calcularResultado(
-      this.numeroPaginas,
-      this.numeroIdiomas
-    );
-    this.precioActualizado.emit(precioTotal);
-  }
+    incrementarIdiomas() {
+        this.numeroIdiomas++;
+        this.actualizarPrecio();
+    }
+
+    decrementarIdiomas() {
+        if (this.numeroIdiomas > 0) {
+            this.numeroIdiomas--;
+            this.actualizarPrecio();
+        }
+    }
+
+    private actualizarPrecio() {
+        const precioTotal = this.resultService.calcularResultado(
+            this.numeroPaginas,
+            this.numeroIdiomas
+        );
+        this.precioActualizado.emit(precioTotal);
+    }
 }
