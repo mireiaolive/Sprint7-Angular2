69b0232a65ebbb9e347c1c2041aca238a2161e40
Mireia Olivé <mirisfu@gmail.com>
2023-05-30 15:27:26 +0200

    exercici-7

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { ResultService } from "../home/services/result.service";
 
 @Component({
     selector: "app-list",
@@ -6,5 +7,30 @@ import { Component, Input } from "@angular/core";
     styleUrls: ["./list.component.css"],
 })
 export class ListComponent {
+    constructor(private resultService: ResultService) {}
     @Input() pressupostos: any[] = [];
+
+    pressupostosFiltrats: any[] = [];
+    ordreAlfabetic: boolean = false;
+    ordreData: boolean = false;
+
+    ordenarAlfabeticament() {
+        this.pressupostos.sort((a, b) => a.nom.localeCompare(b.nom));
+        this.ordreAlfabetic = true;
+        this.ordreData = false;
+    }
+
+    ordenarPerData() {
+        this.pressupostos.sort(
+            (a, b) => new Date(a.data).getTime() - new Date(b.data).getTime()
+        );
+        this.ordreAlfabetic = false;
+        this.ordreData = true;
+    }
+
+    reinicialitzarOrdre() {
+        this.pressupostos = this.resultService.getPressupostos();
+        this.ordreAlfabetic = false;
+        this.ordreData = false;
+    }
 }
